2ac7f5c6c5cf380c640142db4c70c239e2cd3ddc
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-09-24 13:50:40 -0700

    Create NavBar components
    Basic skeleton of the navbar element that will eventually have links to
    the different pages of the store.

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+  return (
+    <div id="navBar">
+      <ul>
+        <li>Home</li>
+        <li>Store</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
