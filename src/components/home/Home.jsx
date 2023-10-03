91ab5e04ff5c91769e9eb7b4b5f8563671b83c54
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-10-03 15:34:45 -0700

    Create and style the home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <div className={styles.homeContainer}>
+        <span>
+          <Link to="../shop" className={styles.homeLink}>
+            Shop Now
+          </Link>
+        </span>
+      </div>
+      <p className={styles.attribution}>Photo by Joshua Chun</p>
+    </>
+  );
 };
 
 export default Home;
