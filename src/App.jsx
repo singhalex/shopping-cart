310851265039b5f9408bd277befc1617d60bd1e9
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-10-06 13:51:15 -0700

    Add updateTotal function to outlet context

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,20 @@ const App = () => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState({});
 
+  const updateTotal = (newCart) => {
+    let runningTotal = 0;
+    let numItems = 0;
+    newCart.forEach((item) => {
+      runningTotal += item.price * item.qty;
+      numItems += item.qty;
+    });
+    setTotal({ grand: Number(runningTotal).toFixed(2), numItems });
+  };
+
   return (
     <>
       <NavBar total={total} />
-      <Outlet context={{ cart, setCart, total, setTotal }} />
+      <Outlet context={{ cart, setCart, total, setTotal, updateTotal }} />
     </>
   );
 };
