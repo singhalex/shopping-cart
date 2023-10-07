31249cfe0d70b55ddc2ffb083470f679e89d927b
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-10-06 20:20:24 -0700

    Style the cart page
    Show each item in a the cart in its own card that is responsive using
    flexbox

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,4 +1,5 @@
 import { useOutletContext } from "react-router-dom";
+import styles from "./Cart.module.css";
 
 const Cart = () => {
   const { cart, setCart, total, updateTotal } = useOutletContext();
@@ -47,20 +48,28 @@ const Cart = () => {
   };
 
   return (
-    <>
+    <div className={styles.container}>
       <h1>Cart Total: ${total.grand}</h1>
       {cart.map((item) => (
-        <div key={item.id}>
+        <div key={item.id} className={styles.itemCard}>
           <img src={item.image} alt={item.description} />
-          <h3>{item.title}</h3>
-          <h4>Qty: {item.qty}</h4>
-          <p>Subtotal: ${Number(item.qty * item.price).toFixed(2)}</p>
-          <button onClick={() => decrementItem(item)}>-</button>
-          <button onClick={() => incrementItem(item)}>+</button>
-          <button onClick={() => deteFromCart(item.id)}>delete</button>
+          <div className={styles.itemInfo}>
+            <h3>{item.title}</h3>
+            <h4>Qty: {item.qty}</h4>
+            <p>Subtotal: ${Number(item.qty * item.price).toFixed(2)}</p>
+            <div className={styles.buttonContainer}>
+              <div className={styles.plusMinusContainer}>
+                <button onClick={() => decrementItem(item)}>-</button>
+                <button onClick={() => incrementItem(item)}>+</button>
+              </div>
+              <button onClick={() => deteFromCart(item.id)}>
+                <img src="/trash.svg?url"></img>
+              </button>
+            </div>
+          </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
