77e7b0aaa5baf05d49b64a18d71cab4cb0e2bd4e
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-10-06 20:38:46 -0700

    Add shopping card icon to navBar

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
 
-const NavBar = () => {
+const NavBar = ({ total }) => {
   return (
     <nav className={styles.nav}>
       <h1 className={styles.siteTitle}>The Store</h1>
@@ -11,6 +11,12 @@ const NavBar = () => {
         </li>
         <li>
           <Link to="shop">Shop</Link>
+        </li>
+        <li>
+          <Link to="cart">
+            ( {total.numItems} )
+            <img className={styles.cartIcon} src="/shopping-cart.png"></img>
+          </Link>
         </li>
       </ul>
     </nav>
