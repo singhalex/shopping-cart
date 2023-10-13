575feacef27b7f6b6dddcb7e3acb2f8ba6421570
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-10-13 14:29:08 -0700

    Make NavBar responsive
    Changed the Links to NavLinks from the react router in order to add
    styling to the links to indicate which page the user is on.
    
    At smaller screen sizes, the links collapse into a hamburger menu which
    slides in from the side.

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,25 +1,53 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./NavBar.module.css";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const NavBar = ({ total }) => {
+  const [showNav, setShowNav] = useState(false);
+
+  const handleShowNavBar = () => {
+    console.log("click");
+    setShowNav(!showNav);
+  };
+
   return (
-    <nav className={styles.nav}>
-      <h1 className={styles.siteTitle}>The Store</h1>
-      <ul>
-        <li>
-          <Link to="home">Home</Link>
-        </li>
-        <li>
-          <Link to="shop">Shop</Link>
-        </li>
-        <li>
-          <Link to="cart">
-            ( {total.numItems} )
-            <img className={styles.cartIcon} src="/shopping-cart.png"></img>
-          </Link>
-        </li>
-      </ul>
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          <h1 className={styles.siteTitle}>The Store</h1>
+        </div>
+        <div className={styles.menuIcon} onClick={handleShowNavBar}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className={`${styles.navElements} ${showNav && styles.active}`}>
+          <ul>
+            <li>
+              <NavLink to="home" onClick={handleShowNavBar}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="shop" onClick={handleShowNavBar}>
+                Shop
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="cart" onClick={handleShowNavBar}>
+                <div className={styles.cart}>
+                  ( {total.numItems} )
+                  <img
+                    className={styles.cartIcon}
+                    src="/shopping-cart.png"
+                  ></img>
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
