3392b8d379f9a7617f732d1707391521b135c5f1
Alex Singh <115970252+singhalex@users.noreply.github.com>
2023-11-01 15:46:54 -0700

    Add dynamic links to the items in the cart
    Also fixed the styles of the links in the cart

--- src/components/cart/CartCard.jsx
+++ src/components/cart/CartCard.jsx
@@ -1,12 +1,21 @@
+import { Link } from "react-router-dom";
 import styles from "./Cart.module.css";
 import PropTypes from "prop-types";
 
 const CartCard = ({ item, decrementItem, incrementItem, deleteFromCart }) => {
   return (
     <div className={styles.itemCard}>
-      <img src={item.image} alt={item.description} />
+      <Link to={`/shop/${item.id}`}>
+        <img
+          src={item.image}
+          alt={item.description}
+          className={styles.itemCard__image}
+        />
+      </Link>
       <div className={styles.itemInfo}>
-        <h3>{item.title}</h3>
+        <Link to={`/shop/${item.id}`}>
+          <h3>{item.title}</h3>
+        </Link>
         <h4>Qty: {item.qty}</h4>
         <p>{`Subtotal: ${Number(
           Number(item.qty * item.price).toFixed(2)
